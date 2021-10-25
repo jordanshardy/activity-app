3f2bfb3930cb292ef79f736a36ca72c94ac6855f
alastairblackwood <agrizzlybear@icloud.com>
2021-10-25 13:14:14 +1000

    api fetch request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Nav from "./Components/Nav";
 import About from "./Components/About";
@@ -6,17 +6,37 @@ import Home from "./Components/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-   return (
-      <Router>
-         <div className="App">
-            <Nav />
-            <Switch>
-               <Route path="/" exact component={Home} />
-               <Route pathh="about" component={About} />
-            </Switch>
-         </div>
-      </Router>
-   );
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="about" component={About} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
+
+useEffect(() => {
+  fetch("https://www.boredapi.com/api/activity")
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw response;
+    })
+    .then((data) => {
+      setData(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data: ", error);
+      setError(error);
+    })
+    .finally(() => {
+      setLoading(false);
+    });
+}, []);
 
 export default App;
